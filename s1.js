4322506cabf6b48fb208f121a20879bf04b7d930
vron7 <63443059+vron7@users.noreply.github.com>
2020-08-08 07:17:11 +0300

    Update s1.js
    Cleanup comment

--- s1.js
+++ s1.js
@@ -1,4 +1,6 @@
-// Clean the room function: given an input of [1,2,4,591,392,391,2,5,10,2,1,1,1,20,20], make a function that organizes these into individual array that is ordered. For example answer(ArrayFromAbove) should return: [[1,1,1,1],[2,2,2], 4,5,10,[20,20], 391, 392,591].
+// Clean the room function: given an input of [1,2,4,591,392,391,2,5,10,2,1,1,1,20,20], 
+// make a function that organizes these into individual array that is ordered. 
+// For example answer(ArrayFromAbove) should return: [[1,1,1,1],[2,2,2], 4,5,10,[20,20], 391, 392,591].
 
 const ul = [1,2,4,591,392,391,2,5,10,2,1,1,1,20,20];
 

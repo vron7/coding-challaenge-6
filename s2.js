ca3e88eeb0e78eb214d06491272a8dcbf6a6a5dc
vron7 <63443059+vron7@users.noreply.github.com>
2020-08-14 14:50:27 +0300

    Add files via upload

---
+++ s2.js
@@ -0,0 +1,14 @@
+// Write a javascript function that takes an array of numbers and a target number. The function should find // two different numbers in the array that, when added together, give the target number. 
+// For example: answer([1,2,3], 4)should return [1,3]
+
+
+const answer = (arr, targetNum) => {
+    for(let num of arr){
+        let num2 = targetNum - num;
+        if(arr.indexOf(num2) >= 0 && num !== num2)
+            return [num, num2];
+    }
+    return [];
+};
+const ul = [2, 1, 2, 3];
+console.log(answer(ul, 4));

7148de36e72113bdbe622e284ae2a7252a430a86
vron <veron.nahkur@pixster.eu>
2020-08-16 07:37:45 +0300

    cleanup

--- s3.js
+++ s3.js
@@ -1,14 +1,11 @@
-// Write a javascript function that takes an array of numbers and a target number. The function should find // two different numbers in the array that, when added together, give the target number. 
-// For example: answer([1,2,3], 4)should return [1,3]
+// Write a function that converts HEX to RGB. 
+// Then Make that function auto-dect the formats so that if you enter HEX color format,
+// it returns RGB and if you enter RGB color format it returns HEX.
 
 
-const convert = (arr, targetNum) => {
-    for(let num of arr){
-        let num2 = targetNum - num;
-        if(arr.indexOf(num2) >= 0 && num !== num2)
-            return [num, num2];
-    }
-    return [];
+const convert = ( colorIn ) => {
+    const colorOut;
+    return colorOut;
 };
-const ul = [2, 1, 2, 3];
-console.log(convert(ul, 4));
+const color = ''#ffffff';
+console.log(convert(color));
